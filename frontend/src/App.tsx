51a12a40680260156cdea7236f660435a48ef586
Julian Cuni <julian.cuni@microservices.al>
2022-09-01 12:04:56 +0200

    Keycloak init. Stale.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
 import type { Component } from "solid-js";
+import type {
+  KeycloakConfig,
+  KeycloakInitOptions,
+} from "@absolid/solid-keycloak";
+import { KeycloakProvider } from "@absolid/solid-keycloak";
 import AppRouter from "./router";
 
+const KEYCLOAK_CONFIG: KeycloakConfig = {
+  clientId: "solidjs",
+  realm: "solidjs",
+  url: "https://oauth.apps.artservis.al/auth",
+};
+
+const KEYCLOAK_INIT_OPTIONS: KeycloakInitOptions = {
+  adapter: "default",
+};
+
 const App: Component = () => {
-  return <AppRouter></AppRouter>;
+  return (
+    <KeycloakProvider
+      config={KEYCLOAK_CONFIG}
+      initOptions={KEYCLOAK_INIT_OPTIONS}
+    >
+      <AppRouter />
+    </KeycloakProvider>
+  );
 };
 
 export default App;
